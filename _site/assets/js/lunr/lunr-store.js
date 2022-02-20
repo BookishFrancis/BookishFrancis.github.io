79f838e1d09faf4ce15bc37693f388a4d6478be7
Francis Mitchell <francis.b.mitchell@gmail.com>
2022-02-20 16:08:07 -0600

    Added author information

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Another test Hello world!  ","categories": [],
+        "tags": [],
+        "url": "/hello-world/",
+        "teaser": null
+      }]
